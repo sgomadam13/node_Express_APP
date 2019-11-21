a18e543b7c050eb01d37ccd6bc8682fb3519109f
Sandhya <20344136+sandyspring13@users.noreply.github.com>
2019-11-21 14:01:33 -0500

    changed process.env.PORT to 3000

--- app.js
+++ app.js
@@ -5,6 +5,6 @@ app.get("/", function(req, res){
     res.render("index.ejs");
 });
 
-app.listen(process.env.PORT, process.env.IP, function(){
+app.listen(3000, process.env.IP, function(){
     console.log("Server has started");
 });
